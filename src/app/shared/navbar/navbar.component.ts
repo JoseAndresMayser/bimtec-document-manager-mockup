f47e6d868a88e582b03730926b1bbf887cdc2306
JoseAndresMayser <lpmayser50@gmail.com>
2023-12-01 18:35:15 -0400

    feat: implemented dropdown to select company

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @Output() public clickOnProject: EventEmitter<void>;
+  @Output() public clickOnCompany: EventEmitter<void>;
 
+  public dropdownOpen: boolean;
+
+  constructor() {
+    this.clickOnProject = new EventEmitter();
+    this.clickOnCompany = new EventEmitter();
+    this.dropdownOpen = false;
+  }
+
+  public goToProjectDetail(): void {
+    this.clickOnProject.emit();
+    this.dropdownOpen = false;
+  }
 }
